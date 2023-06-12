b9826b40824f44b1b4cf87c1fff4d9a2368e7e49
BIJODEV <bijodev1@gmail.com>
2023-06-12 10:50:11 +0530

    Update next-auth.js

--- next-auth.js
+++ next-auth.js
@@ -1,1 +1,15 @@
-
+function handleCredentialResponse(response) {
+    // Handle the response from the credential picker.
+    console.log(response.credential);
+  }
+  
+  google.accounts.id.initialize({
+    client_id: '41790384339-c1tedjaiv70mge0793gm25c242lm3o44.apps.googleusercontent.com',
+    callback: handleCredentialResponse
+  });
+  
+  google.accounts.id.renderButton(
+    document.getElementById('google-signin-button'),
+    { theme: 'outline', size: 'large' }
+  );
+  
